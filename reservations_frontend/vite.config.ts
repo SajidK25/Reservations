7268ac466efb18491f900bd52c00850b726b9de5
sajid khan <sajidk25@gmail.com>
2025-09-24 13:26:00 +0600

    api target changes

--- reservations_frontend/vite.config.ts
+++ reservations_frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://nest-backend:3000",
+        target: "https://reservations-zjhb.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
